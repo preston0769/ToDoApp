c7d1fa19ad379aa77db4b7cd1552123fb04622bc
Preston Zhang <preston0769@gmail.com>
2019-05-23 22:38:40 +1000

    Add some notes

---
+++ ToDo.Api/Controllers/ToDoController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController: ControllerBase
+    {
+        ToDoManager _todoManager;
+        public ToDoController(ToDoManager toDoManager)
+        {
+            _todoManager = toDoManager;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return _todoManager.ToDoList.Select(td => td.ToString());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(Guid id)
+        {
+            return _todoManager.ToDoList.Where(x=>x.Id==id).
+                Select(td => td.ToString()).FirstOrDefault();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ToDo.Api/Startup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Serilog;
+using Serilog.Events;
+
+namespace ToDo.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddScoped<ToDoContext>();
+
+            ConfigureSerilog();
+            services.AddLogging(configure => configure.AddSerilog());
+
+            services.AddScoped<ToDoManager>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+        private void ConfigureSerilog()
+        {
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+        // Add this line:
+        .WriteTo.File(
+            @"C:\temp\LogFiles\ToDoApp\log.txt",
+        fileSizeLimitBytes: 10_000_000,
+        rollOnFileSizeLimit: true,
+        shared: true,
+        flushToDiskInterval: TimeSpan.FromSeconds(1))
+            .CreateLogger();
+
+        }
+    }
+}
